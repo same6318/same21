e863ee7f2a16705a804d0519b7d05d3ce4206ad3
same06 <typewreter@yahoo.co.jp>
2024-06-23 10:42:07 +0900

    update

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,8 +6,13 @@ FactoryBot.define do
 
   #存在しないクラス名をテストデータに作る場合は、class: Taskとクラスの定義が必要。
   factory :second_task, class: Task do
-    title { "メール送信" }
-    content { "顧客へ営業のメールを送る。" }
+    title { "TEST2" }
+    content { "Content2" }
+  end
+
+  factory :third_task, class: Task do
+    title { "TEST3"}
+    content { "Content3" }
   end
 
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
+  # before do
+  #   driven_by(:selenium_chrome_headless)
+  # end
+
   describe '登録機能' do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
@@ -17,14 +21,51 @@ RSpec.describe "Tasks", type: :system do
 
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
-      it '登録済みのタスク一覧が表示される' do
-        #テストで使用するタスクを登録
-        task = FactoryBot.create(:task)
-        #タスク一覧画面に遷移
+      let!(:task) { FactoryBot.create(:task, created_at:"2022-02-18") }
+      let!(:second_task) { FactoryBot.create(:second_task, created_at:"2022-02-17") }
+      let!(:third_task) { FactoryBot.create(:third_task, created_at:"2022-02-16") }
+
+      # it '登録済みのタスク一覧が表示される' do
+      #   #テストで使用するタスクを登録
+      #   task = FactoryBot.create(:task)
+      #   #タスク一覧画面に遷移
+      #   visit tasks_path
+      #   #binding.irb
+      #   #遷移したpageに作成したタスクの文字列がhave_contentしているかをexpectで確認する。
+      #   expect(page).to have_content("Content")
+      # end
+      before do
         visit tasks_path
+        # save_and_open_page
+      end
+
+
+      it "作成済みのタスク一覧が作成日時の降順で表示される" do
+        task_list = all("tbody tr")
+        task_dates = task_list.map do |task| 
+          task.text.match(/\d{4}\/\d{2}\/\d{2}/)[0]
+        end
         #binding.irb
-        #遷移したpageに作成したタスクの文字列がhave_contentしているかをexpectで確認する。
-        expect(page).to have_content("Content")
+        expect(task_dates).to eq ["2022/02/18", "2022/02/17", "2022/02/16"]
+      end
+    end
+
+    context "新たにタスクを作成した場合" do
+      let!(:task) { FactoryBot.create(:task) }
+      let!(:second_task) { FactoryBot.create(:second_task) }
+      let!(:third_task) { FactoryBot.create(:third_task) }
+      before do
+        visit tasks_path
+        # save_and_open_page
+      end
+      it "新しいタスクが一番上に表示される" do
+        tasks = all("tbody tr")
+        task_titles = tasks.map { |task| task.find("td", match: :first).text }
+        #binding.irb
+        expect(task_titles).to eq ["TEST3","TEST2","TEST"]
+        expect(task_titles).not_to eq ["TEST","TEST2","TEST3"]
+        # expect(task_titles).to match %r{#{third_task.title}.*#{second_task.title}.*#{task.title}}
+        # expect(task_titles).not_to match %r{#{task.title}.*#{second_task.title}.*#{third_task.title}}
       end
     end
   end
@@ -34,7 +75,7 @@ RSpec.describe "Tasks", type: :system do
        it 'そのタスクの内容が表示される' do
         task = FactoryBot.create(:second_task)
         visit task_path(task)
-        expect(page).to have_text("メール")
+        expect(page).to have_text("TEST2")
        end
      end
   end
